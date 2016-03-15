f7ed1f9fd2270724159f2fb9d213fa7699a0dedb
codefalling <me@codefalling.com>
2016-03-15 18:48:32 +0800

    complete ques-2

--- ques-2.js
+++ ques-2.js
@@ -3,7 +3,9 @@
 // 请将数字字符串:'123'转化为数字：123：
 var str = '123';
 // TODO:
+var num = Number(str)
 
 // 使用Number函数将数字字符串数组：['1','2']转化为数字数组：[1,2]:
 var ary = ['1','2'];
 // TODO:
+var num_ary = ary.map(function(str){return Number(str);});
