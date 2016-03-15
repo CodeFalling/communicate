828a418d215205342189ee685a51c8f8ec034cb9
codefalling <me@codefalling.com>
2016-03-15 19:04:28 +0800

    complete ques-4

--- ques-4.js
+++ ques-4.js
@@ -1,1 +1,17 @@
+function delegate(parentEl, child, eventName, handler) {
+    var childCollection = parentEl.querySelectorAll(child);
+    // TODO:使用Array.prototype.slice()方法将集合转化为真正的数组
+    var childs = Array.prototype.slice.apply(childCollection);
 
+    parentEl.addEventListener(eventName, function(e) {
+        var eventTarget = e.target;
+
+        if (~childs.indexOf(eventTarget)) {
+            handler(e);
+        }
+    }, false);
+}
+
+delegate(document.querySelector('#test'), 'li.active', 'click', function(e) {
+    console.log(e.target);
+});
