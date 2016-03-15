7cfd6d19193daa6efa9a40b97921b9d84c5f0b4a
codefalling <me@codefalling.com>
2016-03-15 18:22:51 +0800

    complete ques-1

--- ques-1.js
+++ ques-1.js
@@ -26,15 +26,18 @@ var injector = {
     resolve: function(target) {
         // 步骤1：将函数target转化为字符串：
         // TODO:
-        var fnText = ;
+        var fnText = target.toString();
         // 步骤2：健壮性起见，使用正则将函数的注释删除掉：
-        var STRIP_COMMENTS = ;
+        var STRIP_COMMENTS = /(\/\*[\s\S]*\*\/|\/\/.*)/;
         fnText = fnText.replace(STRIP_COMMENTS, '');
         // 步骤3：使用正则解析出函数的函数的依赖参数（字符串形式）：
         // TODO:
-        var FN_ARGS = ;// 解析函数依赖参数正则
+        var FN_DECL = /^function\s*[^\(]*\([^\)]*\)/;// 匹配函数声明的部分
+        var FN_DECL_NOT_ARG = /(^function\s*[^\(]*\(|[\n\)\s])/g; // 函数声明中不是参数也不是`,`的部分
         // TODO:
-        var argDecl = ;// 通过fnText.match()方法取得依赖参数的字符串数组
+        var declText = fnText.match(FN_DECL)[0].replace(FN_DECL_NOT_ARG,'');
+
+        var argDecl = declText.split(',');// 通过fnText.match()方法取得依赖参数的字符串数组
         // 步骤4：将真正依赖的变量存放到args数组中：
         var args = [];
 
